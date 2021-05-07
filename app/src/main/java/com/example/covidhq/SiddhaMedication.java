3b1c6ad403fc1ad4e7b95592287f408ca6d43a21
Roshni-19 <68465601+Roshni-19@users.noreply.github.com>
2021-05-07 22:26:27 +0530

    Initial commit

--- app/src/main/java/com/example/covidhq/SiddhaMedication.java
+++ app/src/main/java/com/example/covidhq/SiddhaMedication.java
@@ -3,11 +3,27 @@ package com.example.covidhq;
 import android.support.v4.app.*;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class SiddhaMedication extends Fragment {
+
+    TextView siddha1, siddha2, siddha3;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_siddha_medication, viewGroup, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        siddha1 = view.findViewById(R.id.siddha1);
+        siddha2 = view.findViewById(R.id.siddha2);
+        siddha3 = view.findViewById(R.id.siddha2);
+
+
+        siddha1.setText(R.string.siddha_medications_1);
+        siddha2.setText(R.string.tabbed_siddha_medication);
+        siddha3.setText(R.string.siddha_medication_2);
     }
 }
