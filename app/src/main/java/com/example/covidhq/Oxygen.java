8464e0e26d7783dda141ab38f92eadd047fc0dff
Roshni-19 <68465601+Roshni-19@users.noreply.github.com>
2021-05-07 22:26:00 +0530

    Initial commit

--- app/src/main/java/com/example/covidhq/Oxygen.java
+++ app/src/main/java/com/example/covidhq/Oxygen.java
@@ -2,9 +2,13 @@ package com.example.covidhq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.text.method.MovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 public class Oxygen extends AppCompatActivity {
@@ -13,8 +17,25 @@ public class Oxygen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
+        TextView link;
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oxygen);
+
+        link = findViewById(R.id.link);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlString = "https://www.digit.in/news/general/online-covid-19-resources-hospital-beds-medicines-oxygen-supply-59297.html";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlString));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.android.chrome");
+                startActivity(intent);
+
+            }
+        });
 
 
 
