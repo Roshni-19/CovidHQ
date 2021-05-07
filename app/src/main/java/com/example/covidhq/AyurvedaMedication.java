f5672d4667bd66503a2cdb74c3255ba40eb99eff
Roshni-19 <68465601+Roshni-19@users.noreply.github.com>
2021-05-07 22:24:12 +0530

    Update AyurvedaMedication.java

--- app/src/main/java/com/example/covidhq/AyurvedaMedication.java
+++ app/src/main/java/com/example/covidhq/AyurvedaMedication.java
@@ -1,13 +1,31 @@
 package com.example.covidhq;
 
-import android.support.v4.app.*;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class AyurvedaMedication extends Fragment {
+
+    TextView ayurvedha1, ayurvedha2, ayurvedha3 ;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+
+
+
         return inflater.inflate(R.layout.activity_ayurveda_medication, viewGroup, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ayurvedha1 = view.findViewById(R.id.ayurvedha1);
+        ayurvedha2 = view.findViewById(R.id.ayurvedha2);
+        ayurvedha3 = view.findViewById(R.id.ayurvedha3);
+
+
+        ayurvedha1.setText(R.string.ayurveda_medication_1);
+        ayurvedha2.setText(R.string.tabbed_ayurveda_medication);
+        ayurvedha3.setText(R.string.ayurveda_medication_2);
     }
 }
